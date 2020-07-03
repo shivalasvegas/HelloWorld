083b23a1b72a101b11031a60feecdbdb13c4616e
shivalasvegas <shiva.king@academytrainee.com>
2020-07-03 10:37:23 +0100

    Added method Scanner (scans in message) in HelloWorld class

--- src/com/qa/helloworld/HelloWorld.java
+++ src/com/qa/helloworld/HelloWorld.java
@@ -1,18 +1,35 @@
 package com.qa.helloworld;
 
-
+import java.util.Scanner;
 
 public class HelloWorld {
+	
+	private String message;
+	private Scanner scanMessage  = new Scanner(System.in);
 	
 	public HelloWorld() {
 		
 	}
+	
+	public HelloWorld (String message) {
 
+		this.message = message;
+	}
+
+	public String getMessage() {
+		// use scanner to get message
+		System.out.println("Please enter your message: ");
+		message = scanMessage.nextLine();
+		return message;
+		
+	}
+	
 	public void greetMessage() {
 	Greeting greetService1 = message ->
     System.out.println("Hello " + message);
-    greetService1.sayMessage("Vin");
+    greetService1.sayMessage("Frank");
 	}
+	
 	public String returnMessage() {
 		String message = "Gulp!";
 		return message;
@@ -28,5 +45,8 @@ public class HelloWorld {
 
 		System.out.println(message);
 	}
+
+	
+	
 
 }
